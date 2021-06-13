faf59c73686dc5ce4f947a2485a238e4765d8010
Emmanuel1237 <emmanuelkiranr@gmail.com>
2021-06-14 02:22:43 +0530

    Changed stylesheet to Styled Components

--- src/App.js
+++ src/App.js
@@ -2,17 +2,19 @@ import React from "react";
 import Appointments from "./Appointments";
 import Availablestaff from "./Availablestaff";
 import Patientcount from "./Patientcount";
+import GlobalStyle from "./theme/globalStyle";
 
-function App() {
-  // render() {
-  return (
-    <div>
-      <Patientcount />
-      <Appointments />
-      <Availablestaff />
-    </div>
-  );
-  // }
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <GlobalStyle />
+        <Patientcount />
+        {/* <Appointments />
+        <Availablestaff /> */}
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Basecard.js
+++ src/Basecard.js
@@ -1,30 +1,23 @@
 import React, { Component } from "react";
+import { Container } from "@material-ui/core";
+import Overlapgroup from "./StyledComponents/Basestyle";
 
 class Basecard extends Component {
   render() {
     return (
-      <div className="container-center-horizontal">
-        <div className={this.props.overlapgroup}>
-          <div className="base-card-component">
-            <div className={this.props.overlay}>
-              <h1 className="count-number">{this.props.count}</h1>
-              <div className="count-label">{this.props.label}</div>
-              <img className="card-icon" src={this.props.icon} alt="" />
-            </div>
+      <Container maxWidth="xs">
+        <Overlapgroup>
+          <h1>{this.props.count}</h1>
+          <label>{this.props.label}</label>
+          <img src={this.props.icon} alt="" />
+          <div>
+            <label>{this.props.pillValue}</label>
+            <img src={this.props.percentageArrow} alt="" />
           </div>
-          <div className="percentage-pill">
-            <div className="base-pill-overlap">
-              <div className="pill-value">{this.props.pillValue}</div>
-              <img
-                className="percentage-arrow-up"
-                src={this.props.percentageArrow}
-                alt=""
-              />
-            </div>
-          </div>
-        </div>
-      </div>
+        </Overlapgroup>
+      </Container>
     );
   }
 }
+
 export default Basecard;
